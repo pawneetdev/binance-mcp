e22f3dcf048346b18328eec63ea9b6b42bfece48
pawneetdev <rubalps@gmail.com>
2025-11-17 23:23:21 +0530

    Weather test MCP; 2 mcps - stdio and streamable-http

--- math_server.py
+++ math_server.py
@@ -4,12 +4,17 @@ mcp = FastMCP("Math")
 
 @mcp.tool
 def add(a: int, b: int) -> int:
+    """
+    Add two numbers
+    """
     return a + b
 
 @mcp.tool
 def multiply(a: int, b: int) -> int:
+    """
+    Multiple two numbers
+    """
     return a * b
 
 if __name__ == "__main__":
-    # mcp.run(tranport="stdio")
-    mcp.run(transport="http", port=8000)
+    mcp.run(transport="stdio")
